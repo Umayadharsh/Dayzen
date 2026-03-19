a3641bf61b8d77d70f404e4b7783b86b436f8466
Umaya <umayadharsh@gmail.com>
2026-03-19 15:44:25 +0530

    initial commit1

--- Backend/server.js
+++ Backend/server.js
@@ -20,6 +20,10 @@ app.use(cors({
   credentials: true
 }));
 
+app.get('/', (req, res) => {
+  res.send('Dayzen API is running 🚀');
+});
+
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
